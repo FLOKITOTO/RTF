e66360a9ccee752f95c5c059a1715af0630cb73c
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-04-08 12:33:41 +0200

    Initial commit

---
+++ src/test/java/com/example/rtff/RtffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rtff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
