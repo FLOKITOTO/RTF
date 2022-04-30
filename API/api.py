d0448e71464bc2848d2c96ce6337118f91e0b415
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-04-30 14:20:10 +0200

    fixed

--- API/api.py
+++ API/api.py
@@ -9,7 +9,7 @@ api = Flask(__name__)
 
 def connect():
     db = pymysql.connect(database='rtap',port=3306, host='rtap.mysql.database.azure.com', user='Flokitoto', password='HgnxrEU7i7ykP4r',ssl_ca="{ca-cert filename}", ssl_disabled=True)
-    log.basicConfig(level=log.DEBUG, format='%(asctime)s %(levelname)s:\n%(message)s\n')
+    log.basicConfig(level=log.DEBUG, format='%(asctime)s %(levelname)s:\n%(message)s\n')              
     print("Connexion réussie")
     return db
 
@@ -34,11 +34,6 @@ def write():
     db = connect()
     cursor = db.cursor()
     req = request.json["response"]
-<<<<<<< HEAD
-=======
-    # cursor.executemany("INSERT INTO `plane` (aircraft_icao, reg_number) VALUES (%s, %s);", req)
-    # print(req)
->>>>>>> ed63d53bff98e7a6401fc1f2f78afd40f068e9ec
     key = []
     for i in req:
         try:
@@ -46,23 +41,15 @@ def write():
                 plane = "INSERT INTO `plane` (aircraft_icao, reg_number) VALUES ('{}', '{}')".format(i["aircraft_icao"], i["reg_number"])
                 key.append(i["reg_number"])
             else:
-<<<<<<< HEAD
-                if(i["reg_number"] not in key) : 
-=======
-                if(i["reg_number"] not in key) : #si la key na pas encore été utilisée, on peut ecrire la requête, sinon on ne fait rien
->>>>>>> ed63d53bff98e7a6401fc1f2f78afd40f068e9ec
+                if(i["reg_number"] not in key) :
                     plane+= ", ('{}', '{}')".format(i["aircraft_icao"], i["reg_number"])
                     key.append(i["reg_number"])
         except Exception as e:
              print("failed")
-<<<<<<< HEAD
+
     cursor.execute(plane)
     db.commit()
     cursor.close()
-=======
-    # print(plane)
-    cursor.execute(plane)
->>>>>>> ed63d53bff98e7a6401fc1f2f78afd40f068e9ec
     return jsonify(req)
 
 if __name__=='__main__':
