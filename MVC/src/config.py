e3e97de22279727df491570b56ddfda8d64f742a
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-02 11:25:02 +0200

    Update config.py

--- MVC/src/config.py
+++ MVC/src/config.py
@@ -2,11 +2,10 @@ import os
 
 SECRET_KEY = os.urandom(32)
 
-# Grabs the folder where the script runs.
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-# Enable debug mode.
+
 DEBUG = True
-# Connect to the database
 
 BDD_USER = 'Flokitoto'
 BDD_PASS = 'HgnxrEU7i7ykP4r'
@@ -15,5 +14,4 @@ BDD_PORT = 3306
 BDD_HOST = 'rtap.mysql.database.azure.com'
 # BDD_SSL = True
 
-# Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
