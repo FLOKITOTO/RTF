d6f456efd57b6ac8341cdae72b4b73c15b95494b
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-03 22:20:05 +0200

    Add files via upload

---
+++ MVC/src/routes/flight_bp.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+from controllers.planeController import store, show
+
+flight_bp = Blueprint('plane_bp', __name__)
+
+
+flight_bp.route('/create', methods=['POST'])(store)
+flight_bp.route('/<string:flight>', methods=['GET'])(show)
