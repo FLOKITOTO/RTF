69a83271bf361633ae70b45a8c88ddc8f19232f2
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-03 22:18:20 +0200

    Update plane.py

--- MVC/src/models/plane.py
+++ MVC/src/models/plane.py
@@ -6,13 +6,14 @@ class Plane(db.Model):
 
     __tablename__ = 'plane'
    
-    reg_number = db.Column(db.String, primary_key=True)
+    reg_number = db.Column(db.String(120))
     aircraft_icao = db.Column(db.String(120))
-
+    id = db.Column(db.Int, primary_key=True) #CHECK opé ici
 
     @property
     def serialize(self):
         return {
             'reg_number': self.reg_number,
             'aircraft_icao': self.aircraft_icao,
+            'id': self.id,
         }
