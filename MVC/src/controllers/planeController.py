69a3a43cbbfc66849926856d7876cc3f9ae25adf
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-03 22:18:00 +0200

    Update planeController.py

--- MVC/src/controllers/planeController.py
+++ MVC/src/controllers/planeController.py
@@ -2,7 +2,8 @@ from flask import jsonify, request
 
 from flask_sqlalchemy import SQLAlchemy
 
-from models.plane import Plane
+# from models.plane import Plane
+# from models.flight import Flight
 
 from utils.connect import Database
 import logging as log
@@ -13,17 +14,9 @@ def store():
     db = Database.connect()
     cursor = db.cursor()
     req = request.json["response"]
-    key = []
     for i in req:
         try:
-            if (key==[]) :
-                plane = "INSERT INTO `plane` (aircraft_icao, reg_number) VALUES ('{}', '{}')".format(i["aircraft_icao"], i["reg_number"])
-                key.append(i["reg_number"])
-            else:
-                if(i["reg_number"] not in key) :
-                    plane+= ", ('{}', '{}')".format(i["aircraft_icao"], i["reg_number"])
-                    key.append(i["reg_number"])
-          
+            plane = "INSERT INTO `plane` (aircraft_icao, reg_number, long, lat) VALUES ('{}', '{}', '{}', '{}')".format(i["aircraft_icao"], i["reg_number"],i["long"],i["lat"])
         except Exception as e:
              print("failed")
 
@@ -32,6 +25,8 @@ def store():
     db.commit()
     cursor.close()
     return jsonify(req)
+
+
 
 def show():
     db = Database.connect()
