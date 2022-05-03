315b06702ca4ec0c10201d97e114378dcb011b6a
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-03 22:21:18 +0200

    Update scrapper.py

--- MVC/src/scrapper.py
+++ MVC/src/scrapper.py
@@ -7,7 +7,7 @@ class ScraperSQL:
     def __init__(self):
         self.headers = {'Content-Type': 'application/json'}
         self.urlConnectStr = "http://localhost:5000/create"
-        self.req = requests.get('https://airlabs.co/api/v9/flights?api_key=7d20049f-9fa2-41ac-82dd-60023d4b5058&_fields=reg_number,aircraft_icao')
+        self.req = requests.get('https://airlabs.co/api/v9/flights?api_key=7d20049f-9fa2-41ac-82dd-60023d4b5058&_fields=reg_number,aircraft_icao,long,lat')
 
     def response_s(self):
         response = requests.post(self.urlConnectStr, json = json.loads(self.req.content))
