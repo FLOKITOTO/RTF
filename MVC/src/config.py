58466448ee36eee462cb80f221d84de17b83f9b5
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-08 12:01:31 +0200

    Update config.py

--- MVC/src/config.py
+++ MVC/src/config.py
@@ -1,17 +1,18 @@
 import os
 
-SECRET_KEY = os.urandom(32)
+class Conf:
+    SECRET_KEY = os.urandom(32)
 
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+    DEBUG = True
 
-DEBUG = True
+    BDD_USER = 'Flokitoto'
+    BDD_PASS = 'HgnxrEU7i7ykP4r'
+    BDD_NAME = 'rtap'
+    BDD_PORT = 3306
+    BDD_HOST = 'rtap.mysql.database.azure.com'
+    # BDD_SSL = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + BDD_NAME
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-BDD_USER = 'Flokitoto'
-BDD_PASS = 'HgnxrEU7i7ykP4r'
-BDD_NAME = 'rtap'
-BDD_PORT = 3306
-BDD_HOST = 'rtap.mysql.database.azure.com'
-# BDD_SSL = True
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
