91f5034c4bd76cc5de8bbf6b132312c8c374fd66
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-08 21:01:47 +0200

    Update plane_bp.py

--- MVC/src/routes/plane_bp.py
+++ MVC/src/routes/plane_bp.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 
-from controllers.planeController import store, show
+from controllers.planeController import store, show, update
 
 plane_bp = Blueprint('plane_bp', __name__)
 
-
 plane_bp.route('/create', methods=['POST'])(store)
 plane_bp.route('/list', methods=['GET'])(show)
+plane_bp.route('/update', methods=['PUT'])(update)
