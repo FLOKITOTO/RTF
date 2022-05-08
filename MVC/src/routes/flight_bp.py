04b9bf01e3bb948a2822cb412cc9dfd6d224cc00
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-08 21:01:32 +0200

    Delete flight_bp.py

--- MVC/src/routes/flight_bp.py
+++
@@ -1,9 +0,0 @@
-#from flask import Blueprint
-
-#from controllers.planeController import store, show
-
-#flight_bp = Blueprint('flight_bp', __name__)
-
-
-#flight_bp.route('/create', methods=['POST'])(store)
-#flight_bp.route('/<string:flight>', methods=['GET'])(show)
