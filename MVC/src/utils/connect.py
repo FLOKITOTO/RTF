2d0d2d3e5d917c8e11e9008675af9b5f2bab5d06
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-05-08 12:00:35 +0200

    Update connect.py

--- MVC/src/utils/connect.py
+++ MVC/src/utils/connect.py
@@ -5,15 +5,16 @@ import logging as log
 
 from sqlalchemy import null
 from config import *
+from config import Conf
 
 class Database: 
     def connect():
         db = pymysql.connect(
-            database=BDD_NAME,
-            port=BDD_PORT,
-            host=BDD_HOST, 
-            user=BDD_PASS, 
-            password=BDD_PASS, 
+            database=Conf.BDD_NAME,
+            port=Conf.BDD_PORT,
+            host=Conf.BDD_HOST, 
+            user=Conf.BDD_USER, 
+            password=Conf.BDD_PASS, 
             ssl_disabled= True)
         log.basicConfig(level=log.DEBUG, format='%(asctime)s %(levelname)s:\n%(message)s\n')              
         print("Connexion réussie")
